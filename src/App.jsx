33787662d005b3c7015d61660b7fa722199ebcaf
Guilherme Guimaraes <gillguimaraes@Guilhermes-MacBook-Pro-2.local>
2023-12-07 14:41:42 -0500

    working on useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react"
+import {useState} from "react"
 import "./styles.css"
 
 
 export default function App(){
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }; //prevent the page from refreshing onSubmit
+
   return (
     <>
       <form className="new-item-form">
             <div className="form-row">
               <label htmlFor="item">New Item</label>
-              <input type="text" id="item"/>
+              <input 
+              value={newItem}
+              onChange={e => setNewItem(e.target.value)} // getting the value of the input, setting as newItem(reRendering component)
+               type="text" 
+               id="item"/>
             </div>
             <button className="btn">Add</button>
         </form>
