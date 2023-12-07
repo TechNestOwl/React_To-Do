a5baa97fce219ae53492a4dd21e7d621d4ae60a9
Guilherme Guimaraes <gillguimaraes@Guilhermes-MacBook-Pro-2.local>
2023-12-07 15:52:45 -0500

    added delete function

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,24 @@ export default function App(){
     setNewItem("") // resetting newItem to empty string to clear input
   }; 
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo=> {
+        if(todo.id === id){
+          //todo.completed  = completed // ** immutable this will not work. 
+          return {...todo, completed} //creating new todo state object and changing completed property
+        }
+        return todo // return current todo incase its not matching current ID 
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => { //grag all my currenttodos
+      return currentTodos.filter(todo => todo.id !== id) // filter all todos except the one I want to delete
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -40,10 +58,13 @@ export default function App(){
               return (
                  <li key={todo.id}>
                   <label>
-                    <input type="checkbox" checked={todo.completed}/>
+                    <input type="checkbox" 
+                    checked={todo.completed}
+                    onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                     {todo.title}  
                   </label>
-                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-danger"
+                   onClick={() => deleteTodo(todo.id)}>Delete</button> // pass with an arrow function. Otherwise 
                 </li>
               )
           } )}
